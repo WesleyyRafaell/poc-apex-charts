3631bf0bd29abfb4fd0dc3cd5d84edf27c4df9cf
Wesley Rafael <wesley.santos@mblabs.com.br>
2024-01-05 16:14:56 -0300

    feat: movigrama

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import './App.css';
 
-import Chart from "react-apexcharts";
 import BarLine from './components/charts/BarLine';
 import Bar from './components/charts/Bar';
+import Movimograma from './components/charts/Movimograma';
 
 function App() {
-  const movigrama = {
-    options: {
-      chart: {
-        toolbar: {
-          show: false
-        },
-        id: "chart"
-      },
-      stroke: {
-        curve: 'stepline',
-      },
-      dataLabels: {
-        enabled: false
-      },
-      title: {
-        text: 'Movigrama',
-        align: 'left'
-      },
-      grid: {
-        show: false,
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 60, 80, 40, 70, 20, 70, 90]
-      }
-    ]
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Chart
-            options={movigrama.options}
-            series={movigrama.series}
-            type="line"
-            width="500"
-          />
+          <Movimograma />
         </div>
         <div style={{width: '100%'}}>
           <Bar />
@@ -52,7 +17,6 @@ function App() {
         <div style={{width: '100%'}}>
           <BarLine />
         </div>
-
       </header>
     </div>
   );
