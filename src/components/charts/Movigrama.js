e15660a12674249d580c84fd7ee4d5c9d74a7872
Wesley Rafael <wesley.santos@mblabs.com.br>
2024-01-10 14:35:20 -0300

    feat: add values

--- src/components/charts/Movigrama.js
+++ src/components/charts/Movigrama.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Chart from "react-apexcharts";
+import { SeriesMovigrama } from "./movigramaSeries";
 
 const Movigrama = () => {
     const data = {
@@ -19,87 +20,25 @@ const Movigrama = () => {
                 y2: 60,
                 color: '#01F707',
             },
-        ],
-        series: [
-        [1327359600000, -500],
-        [1327446000000, 0],
-        [1327532400000, 100.5],
-        [1327618800000, 200.7],
-        [1327878000000, 255],
-        [1327964400000, 375.45],
-        [1328050800000, 600.15],
-        [1328137200000, 1012],
-        [1328223600000, 2500],
-        [1328482800000, 4018.19],
-        [1328569200000, 6487.20],
         ]
     }
-
-    // data.series.map(item => item)
-
-    // [
-    //   {
-    //     x: new Date(1327359600000),
-    //     y: -500,
-    //   },
-    //   {
-    //     x: new Date(1327446000000),
-    //     y: 0
-    //   },
-    //   {
-    //     x: new Date(1327532400000),
-    //     y: 100.5,
-    //   },
-    //   {
-    //     x: new Date(1327618800000),
-    //     y: 200.7
-    //   },
-    //   {
-    //     x: new Date(1327878000000),
-    //     y: 255
-    //   },
-    //   {
-    //     x: new Date(1327964400000),
-    //     y: 375.45
-    //   },
-    //   {
-    //     x: new Date(1328050800000),
-    //     y: 600.15
-    //   },
-    //   {
-    //     x: new Date(1328137200000),
-    //     y: 1012
-    //   },
-    //   {
-    //     x: new Date(1328223600000),
-    //     y: 2500
-    //   },
-    //   {
-    //     x: new Date(1328482800000),
-    //     y: 4018.19
-    //   },
-    //   {
-    //     x: new Date(1328569200000),
-    //     y: 6487.20
-    //   },
-    //   ]
-
     
     const settings = {
-        series: [{
+        series: [
+          {
             name: "Saldo",
             type: "line", 
-            data: [0, 25, 36, 30, 45, 35, 64, 52, 59, -36, 39]
+            data: SeriesMovigrama.balance.map(item => item)
           }, 
           {
             name: 'Entradas',
             type: 'column',
-            data: [30, 0, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+            data: SeriesMovigrama.input.map(item => item)
           },
           {
             name: 'Saidas',
             type: 'column',
-            data: [-30, -40, -22, -50, -13, -85, -70, -21, -44, -60, -30]
+            data: SeriesMovigrama.output.map(item => item)
           },
         ],
           options: {
@@ -151,13 +90,15 @@ const Movigrama = () => {
             },
             xaxis: {
               type: 'categories',
-              categories: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
-                '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
-              ],
+              categories: SeriesMovigrama.day.map(item => item),
+              overwriteCategories: ['teste', 'teste 2'],
               labels: {
                 show: true,
                 rotate: -90,
-                rotateAlways: true,
+                // rotateAlways: true,
+                hideOverlappingLabels: true,
+                showDuplicates: false,
+                trim: true,
               },
               tooltip: {
                 enabled: false,
